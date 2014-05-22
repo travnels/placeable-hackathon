67d120de68c1f627d3a78ebe57ce6368235b65f0
Mark Hengel <mhengel@locationinsight.com>
2014-05-22 10:11:07 -0600

    Super awesomeness

--- js/index.js
+++ js/index.js
@@ -17,93 +17,159 @@ $(function() {
 
     $("#leftButton").live("vmousedown",function() {
         console.log(' Left Button has been clicked');
+        leftButton();
+        
+    });
 
+    $("#leftButton").live("vmouseup",function() {
+        console.log(' Left Button has been unclicked');
+        leftButtonStop();
+        
+    });
+    leftButton = function() {
         $.ajax({
             'url' : '/motorLeftOn',
             'type' : 'PUT'
         });
-    })
-
-    $("#leftButton").live("vmouseup",function() {
-        console.log(' Left Button has been unclicked');
-
+    };
+    leftButtonStop = function() {
         $.ajax({
             'url' : '/motorLeftOff',
             'type' : 'PUT'
         });
-    })
+    };
 
     $("#rightButton").live("vmousedown",function() {
         console.log(' Right Button has been unclicked');
 
+        rightButton();
+    });
+
+    rightButton = function() {
         $.ajax({
             'url' : '/motorRightOn',
             'type' : 'PUT'
         });
-    })
+    };
 
-    $("#rightButton").live("vmouseup",function() {
-        console.log(' Right Button has been unclicked');
-
+    rightButtonStop = function() {
         $.ajax({
             'url' : '/motorRightOff',
             'type' : 'PUT'
         });
-    })
+    };
+
+    $("#rightButton").live("vmouseup",function() {
+        console.log(' Right Button has been unclicked');
+        rightButtonStop();
+    });
 
     $("#upButton").live("vmousedown",function() {
         console.log(' Up Button has been unclicked');
-
+        upButton();
+        
+    });
+    upButton = function() {
         $.ajax({
             'url' : '/motorUpOn',
             'type' : 'PUT'
         });
-    })
-
-    $("#upButton").live("vmouseup",function() {
-        console.log(' Up Button has been unclicked');
-
+    };
+    upButtonStop = function() {
         $.ajax({
             'url' : '/motorUpOff',
             'type' : 'PUT'
         });
-    })
+    };
+
+    $("#upButton").live("vmouseup",function() {
+        console.log(' Up Button has been unclicked');
+        upButtonStop();
+
+    });
 
     $("#downButton").live("vmousedown",function() {
         console.log(' Down Button has been unclicked');
 
-        $.ajax({
-            'url' : '/motorDownOn',
-            'type' : 'PUT'
-        });
-    })
+        downButton();
+    });
 
     $("#downButton").live("vmouseup",function() {
         console.log(' Down Button has been unclicked');
-
+        downButtonStop();
+        
+    })
+    downButtonStop = function() {
         $.ajax({
             'url' : '/motorDownOff',
             'type' : 'PUT'
         });
-    })
+    };
+
+    downButton = function() {
+        $.ajax({
+            'url' : '/motorDownOn',
+            'type' : 'PUT'
+        });
+    };
 
     $("#fireButton").live("vmousedown",function() {
         console.log(' Fire Button has been unclicked');
-
+        fireButton();
+        
+    });
+    fireButton = function() {
         $.ajax({
             'url' : '/fireOn',
             'type' : 'PUT'
         });
-    })
+    };
 
     $("#fireButton").live("vmouseup",function() {
         console.log(' Fire Button has been unclicked');
+        fireButtonStop();
 
+    })
+    fireButtonStop = function() {
         $.ajax({
             'url' : '/fireOff',
             'type' : 'PUT'
         });
-    })
+    };
+
+    $('body').keydown(function(e) {
+        console.log(e);
+        if (e.keyCode == 97 || e.charCode == 97) {
+            leftButton();
+        }
+        if (e.keyCode == 119 || e.charCode == 119) {
+            upButton();
+        }
+        if (e.keyCode == 100 || e.charCode == 100) {
+            rightButton();
+        }
+        if (e.keyCode == 13 || e.charCode == 0) {
+            fireButton();
+        }
+    });
+    $('body').keyup(function(e) {
+        console.log(e);
+        if (e.keyCode == 97 || e.charCode == 97) {
+            leftButtonStop();
+        }
+        if (e.keyCode == 119 || e.charCode == 119) {
+            upButtonStop();
+        }
+        if (e.keyCode == 100 || e.charCode == 100) {
+            rightButtonStop();
+        }
+        if (e.keyCode == 115 || e.charCode == 115) {
+            downButtonStop();
+        }
+        if (e.keyCode == 13 || e.charCode == 0) {
+            fireButtonStop();
+        }
+    });
 
     launchDrone = function() {
         $.ajax({
